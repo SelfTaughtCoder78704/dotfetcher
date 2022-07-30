b7cc854e425739335223756522f9d574adaaa740
SelfTaughtCoder78704 <bobbynicholson78704@gmail.com>
2022-07-30 15:54:52 -0500

    app up

---
+++ main.js
@@ -0,0 +1,33 @@
+// const API_KEY = 'your_api_key'
+// const DOTNUM = '1595946'
+let dotNum = document.getElementById('dotnum')
+let submit = document.getElementById('submit')
+let form = document.getElementById('form')
+// const DemoURL = `https://mobile.fmcsa.dot.gov/qc/services/carriers/name/greyhound?webKey=${API_KEY}`
+// const URL = `https://mobile.fmcsa.dot.gov/qc/services/carriers/${dotNum}?webKey=${API_KEY}`
+// fetch(URL)
+//   .then(response => response.json())
+//   .then(data => { console.log(data) })
+//   .catch(error => { console.log(error) })
+
+let cont = {}
+
+function getTruckInfo(dot, el) {
+  const URL = `https://mobile.fmcsa.dot.gov/qc/services/carriers/${dot}?webKey=${process.env.API_KEY}`
+  fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+      cont = { ...data }
+      el.innerHTML = `<div>${data.content.carrier.legalName}</div>`
+    })
+    .catch(error => { console.log(error) })
+}
+
+submit.addEventListener('click', function (e) {
+  e.preventDefault()
+  dotNum = dotNum.value
+  getTruckInfo(dotNum, form)
+})
+
+
